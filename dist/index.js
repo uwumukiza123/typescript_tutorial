c06b0effd15b2e7d466aa32590aa86e54037ba1a
Laetitia UWUMUKIZA <uwumukizalaetitia@gmail.com>
2023-08-15 11:11:03 +0200

    add intersection types, nullable types, literal types and optional chaining

--- dist/index.js
+++ dist/index.js
@@ -1,6 +1,21 @@
 "use strict";
-let tupple = ['hello', true, 12];
-;
-let mySize = 3;
-console.log(mySize);
+let weight;
+let textBox = {
+    drag: () => { },
+    resize: () => { }
+};
+let height = 50;
+let quantity = 100;
+function greet(name) {
+    if (name)
+        console.log(name.toUpperCase());
+    else
+        console.log('hola');
+}
+greet(null);
+function getCustomer(id) {
+    return id === 0 ? null : { birthday: new Date() };
+}
+let customer = getCustomer(1);
+console.log(customer === null || customer === void 0 ? void 0 : customer.birthday);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -20,3 +20,46 @@ let textBox: UIWidget = {
     drag: () => {},
     resize: () => {}
 }
+
+// 4. LITERAL TYPES 
+//  when we want to limit the value we can assign to a variable
+
+// literal(exact, specific )
+let height: 50 | 100 = 50;
+//   or we can use the type alias
+
+type Quantity = 50 | 100;
+let quantity:   Quantity = 100;
+
+// 5. NULLABLE TYPES
+
+function greet (name: string | null | undefined) {
+    if(name)
+        console.log(name.toUpperCase());
+    else
+        console.log('hola');
+        
+}
+greet(null)
+
+
+// 6. OPTIONAL CHAINING
+type Customer = {
+    birthday?: Date
+};
+
+function getCustomer(id: number): Customer | null | undefined{
+    return id === 0 ? null: {birthday: new Date ()}
+}
+
+let customer = getCustomer(1);
+// if(customer !== null && customer !== undefined)
+// optional property access operator
+    console.log(customer?.birthday?.getFullYear())
+
+// optional element access operator: is good when you are dealing with arrays
+// -> customers?.[0]
+
+// optional call 
+let log: any = null;
+log?.('a')
